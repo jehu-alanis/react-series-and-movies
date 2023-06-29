90aa462c4789f1208c90a77cf2711d99b6a6159f
jehualanis <jehu@onefacture.com>
2023-06-29 16:12:22 -0400

    se modifico el css

--- src/component/card.jsx
+++ src/component/card.jsx
@@ -1,32 +1,28 @@
 import React from "react";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faShoppingBag} from '@fortawesome/free-solid-svg-icons'
-import { Link  } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingBag } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
 
-
- 
-const Card = ({movie}) => {
-return (
-  <div className="col-md-3">
-    <div className="card animated fadeInUp">
-      <img
-        src={movie.Poster}
-        alt={movie.Title}
-        className="card-img-top"
-        width="100"
-      />
-      <div className="card-body">
-        <h4>{movie.Title} {movie.Year}</h4>
-        <p>{movie.Type}</p>
-        <Link to= {`/descripcion/${movie.Title}`}>
-        <FontAwesomeIcon icon={faShoppingBag}></FontAwesomeIcon> 
-        </Link>
+const Card = ({ movie }) => {
+  return (
+    <div className="col-md-3" style={{ maxHeight: '80%',  maxWidth: '100%' }}>
+      <div className="card animated fadeInUp">
+        <img
+          src={movie.Poster}
+          alt={movie.Title}
+          className="card-img-top"
+          style={{ maxWidth: '100%', maxHeight: '100%' }}
+        />
+        <div className="card-body">
+          <h4>{movie.Title} {movie.Year}</h4>
+          <p>{movie.Type}</p>
+          <Link to={`/descripcion/${movie.Title}`}>
+            <FontAwesomeIcon icon={faShoppingBag}></FontAwesomeIcon>
+          </Link>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
 }
-
-
 
 export default Card;
